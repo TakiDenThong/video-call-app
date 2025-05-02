41378e655bb9536ac3484642d6977a0ef186a13d
TakiDenThong <160148749+TakiDenThong@users.noreply.github.com>
2025-05-02 09:46:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname + '/public')); // your HTML + JS files here
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-  socket.on('offer', (offer) => socket.broadcast.emit('offer', offer));
-  socket.on('answer', (answer) => socket.broadcast.emit('answer', answer));
-  socket.on('ice-candidate', (candidate) => socket.broadcast.emit('ice-candidate', candidate));
+  console.log('User connected:', socket.id);
+
+  socket.on('call-user', (data) => {
+    io.to(data.to).emit('incoming-call', {
+      offer: data.offer,
+      from: socket.id,
+    });
+  });
+
+  socket.on('answer-call', (data) => {
+    io.to(data.to).emit('call-answered', {
+      answer: data.answer,
+      from: socket.id,
+    });
+  });
+
+  socket.on('ice-candidate', (data) => {
+    io.to(data.to).emit('ice-candidate', {
+      candidate: data.candidate,
+      from: socket.id,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+server.listen(3000, () => console.log('Server running on http://localhost:3000'));
